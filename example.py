bb044a60809ef69b028fcfcc39b28c0a1e9fb722
Arghun Mousanezhad <arghunmousanezhad@MacBook-Pro-som-tillhor-Arghun.local>
2026-06-07 13:06:51 +0200

    exa 1

---
+++ example.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel;
+
+class User(BaseModel):
+    name: str
+    age: int
+    email: str
+
+# create a user
+user = User(name="arghun", age=31, email="arghun.developer@gmail.com")
+
+print(user.name)
